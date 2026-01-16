470a090936bb745fa216af88b89fa0d8dfc34e98
William Laursen <99247135+wrl6765@users.noreply.github.com>
2026-01-16 15:48:33 +0100

    det johnson baby

---
+++ include/help.h
@@ -0,0 +1,6 @@
+#ifndef HELP_H_
+#define HELP_H_
+
+void displayHelpScreen(int w, int h);
+void help_update();
+#endif /* HELP_H_ */
---
+++ include/menu.h
@@ -0,0 +1,8 @@
+#ifndef MENU_H
+#define MENU_H
+
+
+void display_menu();
+void menu_update();
+
+#endif /* MENU_H */
