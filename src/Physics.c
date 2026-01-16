914680c576d827416128e2ea6e5e36b36269e42b
rugbroedsmad <94697436+rugbroedsmad@users.noreply.github.com>
2026-01-16 10:51:13 +0100

    Implement joystick-based player movement
    Added joystick control for player vertical movement.

--- src/Physics.c
+++ src/Physics.c
@@ -1,7 +1,8 @@
 #include "Physics.h"
 #include "Ascii.h"
 
-player p;
+//----------------player Physics----------------------
+player p; 
 
 void player_init(void){
 	p.x=X_COORDINATE/5;
@@ -13,9 +14,17 @@ void player_init(void){
 }
 
 void update_player(player *p){
+	uint8_t joystick_pressed = joystick_center_pressed();
+	if (joystick_pressed == 0x1 && p->y > Y_MIN){
+		p->ay = -512; //op
+	}
+	else if (joystick_pressed == 0x0 && p->y < Y_MAX){
+		p->ay = 256; //ned
+	}
 	p->score+=1;
-
-
+	p->vy += p->ay;
+	p->y += p->vy;	
 }
+
 
 
