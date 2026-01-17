65711275fb48dd1332353284062123b83f8f16f7
rugbroedsmad <94697436+rugbroedsmad@users.noreply.github.com>
2026-01-17 14:46:18 +0100

    Update menu.h

--- include/menu.h
+++ include/menu.h
@@ -1,8 +1,7 @@
 #ifndef MENU_H
 #define MENU_H
-
-
-void display_menu();
-void menu_update();
+#include "Game_state.h"
+void display_menu(void);
+void menu_update(GameContext *ctx, uint8_t joystick);
 
 #endif /* MENU_H */
