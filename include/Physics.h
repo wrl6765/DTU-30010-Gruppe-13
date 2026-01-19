0168158e261d7d261e26d9d4a7953a809c1b30ea
Nymand123 <nikolajnymand@gmail.com>
2026-01-19 15:59:02 +0100

    Update Physics.h

--- include/Physics.h
+++ include/Physics.h
@@ -1,53 +1,23 @@
 #ifndef PHYSICS_H_
 #define PHYSICS_H_
-#include "bsp/30010_io.h"
-#include "Game_state.h"
+#include "30010_io.h"
+
 typedef struct{
 	int16_t x, y, vy, ay, score, highscore;
+	int16_t prev_x, prev_y;
 	uint8_t hp;
 	uint8_t alien_level;
 }player;
-
 
 // Bullet struct
 typedef struct{
 	int16_t x, y;   // 8.8
 	uint8_t type;   // 1=regular, 2=bouncing, 3=cannonball, 4=sniper
 	uint8_t size;   // 1=small, 2=medium, 3=large
-} simple_bullet;
+}bullet;
 
-// Bouncing bullet struct - bounces at frame edges with random chance spawn
-typedef struct {
-	int x, y;
-	int vx, vy;
-	int alive;
-	int type;  // 0=normal (dies at edge), 1=bouncing (reflects at edge)
-	int ax,ay;
-} BouncingBullet;
+void update_player(player *p);
 
-
-typedef struct Cannonball
-{
-	int16_t x,y;
-	int16_t type;
-	int16_t vy,vx;
-	int16_t ax,ay;
-} cannonball;
-
-typedef struct sniper
-{
-	int16_t x,y;
-	int16_t type;
-	int16_t vy,vx;
-	int16_t ax,ay;
-} sniper;
-
-
-
-extern player p;
-
-void update_player(player *p, uint8_t joystick, GameContext *ctx);
-
-void player_init(void);
+void player_init(player *p);
 
 #endif /* PHYSICS_H_ */
