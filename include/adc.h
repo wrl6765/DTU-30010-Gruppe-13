8a7510627f31699632845a9fa7c2608a068bf453
SafijaSaumtally <safija211005@gmail.com>
2026-01-20 13:26:04 +0100

    Create adc.h

---
+++ include/adc.h
@@ -0,0 +1,16 @@
+#ifndef ADC_H_
+#define ADC_H_
+
+#include <stdint.h>
+#include "stm32f30x_conf.h"
+
+// Initialiserer ADC og GPIO
+void init_adc(void);
+
+// Læser ADC-værdi (0–4095) fra valgt kanal 8 og 9 (pins CP2 CP3) 
+uint16_t read_adc(uint8_t channel);
+
+// Læser ADC og printer værdi
+void update_display_with_adc(void);
+
+#endif /* ADC_H_ */
